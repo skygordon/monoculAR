e3253bc04cfc51b828c172447471417cd15e1110
Skylar Gordon <47067071+skygordon@users.noreply.github.com>
2019-05-02 12:28:30 -0400

    Add files via upload
    includes step counter code but needs step counter display

--- support_functions.ino
+++ support_functions.ino
@@ -127,6 +127,48 @@ inline unsigned char b64_lookup(char c) {
   return -1;
 }
 
+/*----------------------------------
+ * do_http_request Function:
+ * Arguments:
+ *    char* host: null-terminated char-array containing host to connect to
+ *    char* request: null-terminated char-arry containing properly formatted HTTP request
+ *    char* response: char-array used as output for function to contain response
+ *    uint16_t response_size: size of response buffer (in bytes)
+ *    uint16_t response_timeout: duration we'll wait (in ms) for a response from server
+ *    uint8_t serial: used for printing debug information to terminal (true prints, false doesn't)
+ * Return value:
+ *    void (none)
+ */
+void do_http_request(char* host, char* request, char* response, uint16_t response_size, uint16_t response_timeout, uint8_t serial){
+  WiFiClient client; //instantiate a client object
+  if (client.connect(host, 80)) { //try to connect to host on port 80
+    if (serial) Serial.print(request);//Can do one-line if statements in C without curly braces
+    client.print(request);
+    memset(response, 0, response_size); //Null out (0 is the value of the null terminator '\0') entire buffer
+    uint32_t count = millis();
+    while (client.connected()) { //while we remain connected read out data coming back
+      client.readBytesUntil('\n',response,response_size);
+      if (serial) Serial.println(response);
+      if (strcmp(response,"\r")==0) { //found a blank line!
+        break;
+      }
+      memset(response, 0, response_size);
+      if (millis()-count>response_timeout) break;
+    }
+    memset(response, 0, response_size);  
+    count = millis();
+    while (client.available()) { //read out remaining text (body of response)
+      char_append(response,client.read(),OUT_BUFFER_SIZE);
+    }
+    if (serial) Serial.println(response);
+    client.stop();
+    if (serial) Serial.println("-----------");  
+  }else{
+    if (serial) Serial.println("connection failed :/");
+    if (serial) Serial.println("wait 0.5 sec...");
+    client.stop();
+  }
+}        
 
 uint8_t char_append(char* buff, char c, uint16_t buff_size) {
   int len = strlen(buff);
@@ -134,4 +176,39 @@ uint8_t char_append(char* buff, char c, uint16_t buff_size) {
   buff[len] = c;
   buff[len + 1] = '\0';
   return true;
+}
+
+//hand in a discharge amount, and generate a voltage
+double voltage_from_discharge(double discharge) {
+  double volt = -5.08321149 * discharge + 8.16962756;
+  volt = volt * discharge - 3.53049671;
+  volt = volt * discharge - 0.3295403;
+  volt = volt * discharge + 4.08151442;
+  return volt;
+}
+
+
+///nice binary-search...much quicker!
+double discharge_from_voltage(double voltage, double error){
+  if (voltage > 4.08){
+    return 0.0;
+  }
+  if (voltage < 3.31){
+    return 1.0;
+  }
+  double estimated_discharge = 0.5;
+  double top_bounds = 1.0;
+  double bottom_bounds = 0.0;
+  double estimated_volts = voltage_from_discharge(estimated_discharge);
+  while (fabs(estimated_volts- voltage) > error){
+    if (estimated_volts > voltage){
+      bottom_bounds = estimated_discharge;
+    }
+    else{
+      top_bounds = estimated_discharge;
+    }
+    estimated_discharge = (bottom_bounds + top_bounds)/2;
+    estimated_volts =  voltage_from_discharge(estimated_discharge);
+  } 
+  return estimated_discharge;
 }
