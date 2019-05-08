273341520781c0d67b0b87e5fcd64220f27e5580
Kevin Ren <kevinren99@gmail.com>
2019-05-08 09:43:21 -0400

    Yelp and Shuttle Client-side Rendering
    I updated the client-side interface for the Yelp and Shuttle information retrieval. I also slightly updated the constant display, including changing the font.

--- googlespeechxdisplay/googlespeechxdisplay.ino
+++ googlespeechxdisplay/googlespeechxdisplay.ino
@@ -16,13 +16,16 @@ const int SAMPLE_FREQ = 8000;                          // Hz, telephone sample r
 const int SAMPLE_DURATION = 5;                        // duration of fixed sampling (seconds)
 const int NUM_SAMPLES = SAMPLE_FREQ * SAMPLE_DURATION;  // number of of samples
 const int ENC_LEN = (NUM_SAMPLES + 2 - ((NUM_SAMPLES + 2) % 3)) / 3 * 4;  // Encoded length of clip
+const int STORAGE_SIZE = 40;
 
 const uint16_t RESPONSE_TIMEOUT = 6000;
 const uint16_t OUT_BUFFER_SIZE = 1000; //size of buffer to hold HTTP response
 const uint16_t IN_BUFFER_SIZE = 100;
 char speech_response[OUT_BUFFER_SIZE]; //char array buffer to hold HTTP request for Google Speech
-char display_response[OUT_BUFFER_SIZE]; //char array buffer to hold HTTP response for serverside
+char response[OUT_BUFFER_SIZE]; //char array buffer to hold HTTP response for serverside
 char request[IN_BUFFER_SIZE];
+
+char storage[STORAGE_SIZE];
 
 const int DISPLAY_UPDATE = 30000; //30 second delay between each call for time
 uint32_t display_timer ;
@@ -52,7 +55,7 @@ uint32_t timer;
 bool hungry;
 bool shuttle;
 
-WiFiClientSecure client; //global WiFiClient Secure object 
+WiFiClientSecure client; //global WiFiClient Secure object
 
 HardwareSerial gps_serial(2);
 TinyGPSPlus gps; //gps object
@@ -95,28 +98,28 @@ void setup() {
 }
 
 //main body of code
-void loop(){
-  if (gps_serial.available()){
-    while (gps_serial.available()) 
+void loop() {
+  if (gps_serial.available()) {
+    while (gps_serial.available())
       gps.encode(gps_serial.read());      // Check GPS
   }
-  
+
   //in case of speech
   button_state = digitalRead(PIN_1);
-  if (!button_state && button_state != old_button_state){
+  if (!button_state && button_state != old_button_state) {
     doest_thou_speak();
   }
   old_button_state = button_state;
 
   //maintain display every 30s
-  if (millis() - display_timer > DISPLAY_UPDATE){
+  if (millis() - display_timer > DISPLAY_UPDATE) {
     update_info_http();
-    display_timer = millis();  
+    display_timer = millis();
   }
 }
 
 
-void doest_thou_speak(){
+void doest_thou_speak() {
   Serial.println("listening...");
   record_audio();
   Serial.println("sending...");
@@ -193,11 +196,11 @@ void doest_thou_speak(){
       char* foll_coll = strstr(trans_id, ":");
       char* starto = foll_coll + 2; //starting index
       char* endo = strstr(starto + 1, "\""); //ending index
-      int transcript_len = endo-starto+1;
+      int transcript_len = endo - starto + 1;
       char transcript[100] = {0};
-      strncat(transcript,starto,transcript_len);
+      strncat(transcript, starto, transcript_len);
       Serial.println(transcript);
-      
+
       const char hungryupper[10] = "Hungry";
       const char hungrylower[10] = "hungry";
       char *hung = strstr(transcript, hungryupper); //looks to see if someone said "Hungry"
@@ -213,34 +216,116 @@ void doest_thou_speak(){
       const char shuttlelower[10] = "shuttle";
       char *shutt = strstr(transcript, shuttleupper); //looks to see if someone said "Shuttle"
       char *shut = strstr(transcript, shuttlelower); //looks to see if someone said "shuttle"
-      
-      if ((hung!=NULL)||(hun!=NULL)||(foodd!=NULL)||(food!=NULL)){
+
+      if ((hung != NULL) || (hun != NULL) || (foodd != NULL) || (food != NULL)) {
         hungry = true;
         Serial.println("Found Hungry or hungry or Food or food!!");
-       }
-      if ((shutt!=NULL)||(shut!=NULL)){
+      }
+      if ((shutt != NULL) || (shut != NULL)) {
         shuttle = true;
         Serial.println("Found Shuttle or shuttle!!");
-       }
+      }
 
-      if (hungry&&shuttle){
+      if (hungry && shuttle) {
         Serial.println("Please only give one command at a time.");
         hungry = false;
         shuttle = false;
-       } else if (hungry){
+      } else if (hungry) {
+        get_restaurants();
         Serial.println("Here's some restaurants near you");
         hungry = false;
-       } else if (shuttle){
+      } else if (shuttle) {
+        get_shuttles();
         Serial.println("Here's the MIT shuttle schedule");
         shuttle = false;
-       } else {
+      } else {
         Serial.println("Sorry, I didn't hear a command. Try asking again by holding the button.");
-       }         
+      }
     }
     Serial.println("-----------");
     client.stop();
     Serial.println("done");
   }
+}
+
+void get_restaurants() {
+  sprintf(request, "GET /sandbox/sc/kevinren/monocular/monocular.py?type=yelp&lat=%f&lon=%f HTTP/1.1\r\n",
+          gps.location.lat(), gps.location.lng());
+  strcat(request, "Host: 608dev.net\r\n\r\n");
+  do_http_request("608dev.net", request, response, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT, true);
+  char *x = strtok (response, "\n");
+  int num = atoi(x);
+  int pos_y = 24;
+
+  u8g2.setDrawColor(0);
+  u8g2.drawBox(0, 17, 150, 100);
+  u8g2.setDrawColor(1);
+
+  u8g2.setCursor(5, pos_y);
+  for (int i = 0; i < num; i++) {
+    char *rest_name = strtok (NULL, "\n");
+    char *star_str = strtok(NULL, "\n");
+    int stars = star_str[0] - '0';
+    bool half_star = (star_str[1] == '.' && star_str[2] == '5');
+    char *type = strtok(NULL, "\n");
+    char *loc = strtok(NULL, "\n");
+    char *dist = strtok(NULL, "\n");
+    u8g2.setFont(u8g2_font_5x7_tf);
+    memset(storage, 0, STORAGE_SIZE);
+    snprintf(storage, 18, "%d. %s", i + 1, rest_name);
+    u8g2.print(storage);
+
+    u8g2.setFont(u8g2_font_unifont_t_symbols);
+    for (int j = 0; j < stars; j++) {
+      u8g2.drawGlyph(90 + 7 * j, pos_y + 3, 0x2605);
+    }
+    if (half_star) {
+      u8g2.drawGlyph(90 + 7 * stars, pos_y + 3, 0x2605);
+      u8g2.setDrawColor(0);
+      u8g2.drawBox(95 + 7 * stars, pos_y - 6, 3, 7);
+      u8g2.setDrawColor(1);
+    }
+    pos_y += 7;
+    u8g2.setFont(u8g2_font_u8glib_4_tf);
+    u8g2.setCursor(5, pos_y);
+    memset(storage, 0, STORAGE_SIZE);
+    snprintf(storage, 35, "%s, %s", type, loc);
+    u8g2.print(storage);
+    pos_y += 8;
+    u8g2.setCursor(5, pos_y);
+  }
+  u8g2.sendBuffer();
+}
+
+void get_shuttles() {
+  sprintf(request, "GET /sandbox/sc/kevinren/monocular/monocular.py?type=shuttle&lat=%f&lon=%f HTTP/1.1\r\n",
+          gps.location.lat(), gps.location.lng());
+  strcat(request, "Host: 608dev.net\r\n\r\n");
+  do_http_request("608dev.net", request, response, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT, true);
+
+  u8g2.setDrawColor(0);
+  u8g2.drawBox(0, 17, 150, 100);
+  u8g2.setDrawColor(1);
+
+  char *stop_name = strtok (response, "\n");
+  if (strcmp(stop_name, "0") == 0) {
+    u8g2.drawStr(10, 24, "Error: No shuttles");
+  } else {
+    int pos_y = 24;
+    u8g2.setCursor(5, pos_y);
+    u8g2.setFont(u8g2_font_5x7_tf);
+    memset(storage, 0, STORAGE_SIZE);
+    snprintf(storage, 25, "%s", stop_name);
+    u8g2.print(storage);
+    char *pch = strtok(NULL, "\n");
+    while (pch != NULL) {
+      pos_y += 10;
+      u8g2.setCursor(5, pos_y);
+      u8g2.print(pch);
+      pch = strtok(NULL, "\n");
+    }
+  }
+  u8g2.sendBuffer();
 }
 
 //function used to record audio at sample rate for a fixed nmber of samples
@@ -258,7 +343,7 @@ void record_audio() {
   uint32_t start = millis();
   time_since_sample = micros();
   uint8_t button = digitalRead(PIN_1);
-  while ((sample_num < NUM_SAMPLES)&&(!button)) { //read in NUM_SAMPLES worth of audio data
+  while ((sample_num < NUM_SAMPLES) && (!button)) { //read in NUM_SAMPLES worth of audio data
     value = analogRead(AUDIO_IN);  //make measurement
     raw_samples[sample_num % 3] = mulaw_encode(value - 1241); //remove 1.0V offset (from 12 bit reading)
     sample_num++;
@@ -280,84 +365,88 @@ void record_audio() {
 
 int8_t mulaw_encode(int16_t sample) {
   const uint16_t MULAW_MAX = 0x1FFF;
-   const uint16_t MULAW_BIAS = 33;
-   uint16_t mask = 0x1000;
-   uint8_t sign = 0;
-   uint8_t position = 12;
-   uint8_t lsb = 0;
-   if (sample < 0)
-   {
-      sample = -sample;
-      sign = 0x80;
-   }
-   sample += MULAW_BIAS;
-   if (sample > MULAW_MAX)
-   {
-      sample = MULAW_MAX;
-   }
-   for (; ((sample & mask) != mask && position >= 5); mask >>= 1, position--)
-        ;
-   lsb = (sample >> (position - 4)) & 0x0f;
-   return (~(sign | ((position - 5) << 4) | lsb));
+  const uint16_t MULAW_BIAS = 33;
+  uint16_t mask = 0x1000;
+  uint8_t sign = 0;
+  uint8_t position = 12;
+  uint8_t lsb = 0;
+  if (sample < 0)
+  {
+    sample = -sample;
+    sign = 0x80;
+  }
+  sample += MULAW_BIAS;
+  if (sample > MULAW_MAX)
+  {
+    sample = MULAW_MAX;
+  }
+  for (; ((sample & mask) != mask && position >= 5); mask >>= 1, position--)
+    ;
+  lsb = (sample >> (position - 4)) & 0x0f;
+  return (~(sign | ((position - 5) << 4) | lsb));
 }
 
 /*----------------------------------
- * update_info_http Function:
- * makes pull request to get header display information
- */
-void update_info_http(){
+   update_info_http Function:
+   makes pull request to get header display information
+*/
+void update_info_http() {
   char body[200]; //for body;
-  sprintf(body,"lat=%f&lon=%f",gps.location.lat(),gps.location.lng());//generate body, posting to User, 1 step
+  sprintf(body, "lat=%f&lon=%f", gps.location.lat(), gps.location.lng()); //generate body, posting to User, 1 step
   int body_len = strlen(body); //calculate body length (for header reporting)
   memset(request, sizeof(request), 0);
-  sprintf(request,"POST http://608dev.net/sandbox/sc/carolpan/monocular/info_display.py HTTP/1.1\r\n");
-  strcat(request,"Host: 608dev.net\r\n");
-  strcat(request,"Content-Type: application/x-www-form-urlencoded\r\n");
-  sprintf(request+strlen(request),"Content-Length: %d\r\n", body_len); //append string formatted to end of request buffer
-  strcat(request,"\r\n"); //new line from header to body
-  strcat(request,body); //body
-  strcat(request,"\r\n"); //header
+  sprintf(request, "POST http://608dev.net/sandbox/sc/carolpan/monocular/info_display.py HTTP/1.1\r\n");
+  strcat(request, "Host: 608dev.net\r\n");
+  strcat(request, "Content-Type: application/x-www-form-urlencoded\r\n");
+  sprintf(request + strlen(request), "Content-Length: %d\r\n", body_len); //append string formatted to end of request buffer
+  strcat(request, "\r\n"); //new line from header to body
+  strcat(request, body); //body
+  strcat(request, "\r\n"); //header
   Serial.println(request);
-  do_http_request("608dev.net", request, display_response, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT,true);
-  Serial.println(display_response);
-  char* t = strtok(display_response,"&");
+  do_http_request("608dev.net", request, response, OUT_BUFFER_SIZE, RESPONSE_TIMEOUT, true);
+  Serial.println(response);
+  char* t = strtok(response, "&");
   char* d = strtok(NULL, "&");
   char* icon = strtok(NULL, "&");
-  char* pch = strtok(t,"=");
-  t = strtok(NULL,"=");
-  pch = strtok(d,"=");
-  d = strtok(NULL,"=");
-  pch = strtok(icon,"=");
-  icon = strtok(NULL,"=");
-  u8g2.clearBuffer();
-  if (strcmp(icon,"cloud")==0){
+  char* pch = strtok(t, "=");
+  t = strtok(NULL, "=");
+  pch = strtok(d, "=");
+  d = strtok(NULL, "=");
+  pch = strtok(icon, "=");
+  icon = strtok(NULL, "=");
+  //u8g2.clearBuffer();
+  u8g2.setDrawColor(0);
+  u8g2.drawBox(0, 0, 150, 17);
+  u8g2.setDrawColor(1);
+  u8g2.setFont(u8g2_font_5x7_tf);
+  if (strcmp(icon, "cloud") == 0) {
     drawCloud();
   }
-  if (strcmp(icon,"sun")==0){
+  if (strcmp(icon, "sun") == 0) {
     drawSun();
   }
-  if (strcmp(icon,"moon")==0){
+  if (strcmp(icon, "moon") == 0) {
     drawMoon();
   }
   uint32_t start = micros(); //MARK BEGINNING OF TIMING!
   float voltage = analogRead(A6) * 2 * 3.3 / 4096; // get Battery Voltage (mV)
   float discharge_amt = discharge_from_voltage(voltage, 0.001); //get discharge amount using battery voltage
   drawBattery(1.0 - discharge_amt);
-  u8g2.drawStr(28,13,d);
-  u8g2.drawStr(68,13,t);
+  u8g2.drawStr(27, 13, d);
+  u8g2.drawStr(62, 13, t);
   u8g2.sendBuffer();
 }
 
 
-void drawSun(){
-  u8g2.clearBuffer();          // clear the internal memory
+void drawSun() {
+  //u8g2.clearBuffer();          // clear the internal memory
   u8g2.drawDisc(15, 8, 3, U8G2_DRAW_ALL);
   for (uint8_t i = 0; i < 8; i++) {
     u8g2.drawLine(15 + 5 * sin(i * 2 * PI / 8), 8 - 5 * cos(i * 2 * PI / 8), 15 + 6 * sin(i * 2 * PI / 8), 8 - 6 * cos(i * 2 * PI / 8));
   }
 }
 
-void drawMoon(){
+void drawMoon() {
   u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
   u8g2.drawDisc(13, 8, 7, U8G2_DRAW_ALL);
   u8g2.setDrawColor(0);
@@ -365,18 +454,18 @@ void drawMoon(){
   u8g2.setDrawColor(1);
 }
 
-void drawCloud(){
+void drawCloud() {
   u8g2.drawDisc(11, 11, 3, U8G2_DRAW_ALL);
   u8g2.drawDisc(19, 11, 3, U8G2_DRAW_ALL);
   u8g2.drawDisc(15, 7, 3, U8G2_DRAW_ALL);
   u8g2.drawDisc(15, 10, 3, U8G2_DRAW_ALL);
 }
 
-void drawBattery(float level){
-  u8g2.drawLine(104,5,104,13);
-  u8g2.drawLine(104,5,124,5);
-  u8g2.drawLine(124,5,124,13);
-  u8g2.drawLine(104,13,124,13);
-  u8g2.drawBox(104+20,7,2,4);
-  u8g2.drawBox(104,5,20*level,8);
+void drawBattery(float level) {
+  u8g2.drawLine(104, 5, 104, 13);
+  u8g2.drawLine(104, 5, 124, 5);
+  u8g2.drawLine(124, 5, 124, 13);
+  u8g2.drawLine(104, 13, 124, 13);
+  u8g2.drawBox(104 + 20, 7, 2, 4);
+  u8g2.drawBox(104, 5, 20 * level, 8);
 }
