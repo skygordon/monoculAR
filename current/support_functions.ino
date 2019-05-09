32e24960261b0d5c849d0e2c779c1020326b2d00
Skylar Gordon <47067071+skygordon@users.noreply.github.com>
2019-05-09 15:54:01 -0400

    i couldn't get my code to merge me n casey have been trying for an hour here
      this is it all updated except carol's most recent changes (idk whatever she did 2 hours ago but it doesnt have those) the stepcounter n GET for step requests is up n running the post is just broken.

---
+++ current/support_functions.ino
@@ -0,0 +1,214 @@
+const char PROGMEM b64_alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "abcdefghijklmnopqrstuvwxyz"
+    "0123456789+/";
+/* 'Private' declarations */
+inline void a3_to_a4(unsigned char * a4, unsigned char * a3);
+inline void a4_to_a3(unsigned char * a3, unsigned char * a4);
+inline unsigned char b64_lookup(char c);
+
+int base64_encode(char *output, char *input, int inputLen) {
+  int i = 0, j = 0;
+  int encLen = 0;
+  unsigned char a3[3];
+  unsigned char a4[4];
+
+  while(inputLen--) {
+    a3[i++] = *(input++);
+    if(i == 3) {
+      a3_to_a4(a4, a3);
+
+      for(i = 0; i < 4; i++) {
+        output[encLen++] = pgm_read_byte(&b64_alphabet[a4[i]]);
+      }
+
+      i = 0;
+    }
+  }
+
+  if(i) {
+    for(j = i; j < 3; j++) {
+      a3[j] = '\0';
+    }
+
+    a3_to_a4(a4, a3);
+
+    for(j = 0; j < i + 1; j++) {
+      output[encLen++] = pgm_read_byte(&b64_alphabet[a4[j]]);
+    }
+
+    while((i++ < 3)) {
+      output[encLen++] = '=';
+    }
+  }
+//  output[encLen] = '\0';
+  return encLen;
+}
+
+int base64_decode(char * output, char * input, int inputLen) {
+  int i = 0, j = 0;
+  int decLen = 0;
+  unsigned char a3[3];
+  unsigned char a4[4];
+
+
+  while (inputLen--) {
+    if(*input == '=') {
+      break;
+    }
+
+    a4[i++] = *(input++);
+    if (i == 4) {
+      for (i = 0; i <4; i++) {
+        a4[i] = b64_lookup(a4[i]);
+      }
+
+      a4_to_a3(a3,a4);
+
+      for (i = 0; i < 3; i++) {
+        output[decLen++] = a3[i];
+      }
+      i = 0;
+    }
+  }
+
+  if (i) {
+    for (j = i; j < 4; j++) {
+      a4[j] = '\0';
+    }
+
+    for (j = 0; j <4; j++) {
+      a4[j] = b64_lookup(a4[j]);
+    }
+
+    a4_to_a3(a3,a4);
+
+    for (j = 0; j < i - 1; j++) {
+      output[decLen++] = a3[j];
+    }
+  }
+  output[decLen] = '\0';
+  return decLen;
+}
+
+int base64_enc_len(int plainLen) {
+  int n = plainLen;
+  return (n + 2 - ((n + 2) % 3)) / 3 * 4;
+}
+
+int base64_dec_len(char * input, int inputLen) {
+  int i = 0;
+  int numEq = 0;
+  for(i = inputLen - 1; input[i] == '='; i--) {
+    numEq++;
+  }
+
+  return ((6 * inputLen) / 8) - numEq;
+}
+
+inline void a3_to_a4(unsigned char * a4, unsigned char * a3) {
+  a4[0] = (a3[0] & 0xfc) >> 2;
+  a4[1] = ((a3[0] & 0x03) << 4) + ((a3[1] & 0xf0) >> 4);
+  a4[2] = ((a3[1] & 0x0f) << 2) + ((a3[2] & 0xc0) >> 6);
+  a4[3] = (a3[2] & 0x3f);
+}
+
+inline void a4_to_a3(unsigned char * a3, unsigned char * a4) {
+  a3[0] = (a4[0] << 2) + ((a4[1] & 0x30) >> 4);
+  a3[1] = ((a4[1] & 0xf) << 4) + ((a4[2] & 0x3c) >> 2);
+  a3[2] = ((a4[2] & 0x3) << 6) + a4[3];
+}
+
+inline unsigned char b64_lookup(char c) {
+  if(c >='A' && c <='Z') return c - 'A';
+  if(c >='a' && c <='z') return c - 71;
+  if(c >='0' && c <='9') return c + 4;
+  if(c == '+') return 62;
+  if(c == '/') return 63;
+  return -1;
+}
+
+/*----------------------------------
+ * do_http_request Function:
+ * Arguments:
+ *    char* host: null-terminated char-array containing host to connect to
+ *    char* request: null-terminated char-arry containing properly formatted HTTP request
+ *    char* response: char-array used as output for function to contain response
+ *    uint16_t response_size: size of response buffer (in bytes)
+ *    uint16_t response_timeout: duration we'll wait (in ms) for a response from server
+ *    uint8_t serial: used for printing debug information to terminal (true prints, false doesn't)
+ * Return value:
+ *    void (none)
+ */
+void do_http_request(char* host, char* request, char* response, uint16_t response_size, uint16_t response_timeout, uint8_t serial){
+  WiFiClient client; //instantiate a client object
+  if (client.connect(host, 80)) { //try to connect to host on port 80
+    if (serial) Serial.print(request);//Can do one-line if statements in C without curly braces
+    client.print(request);
+    memset(response, 0, response_size); //Null out (0 is the value of the null terminator '\0') entire buffer
+    uint32_t count = millis();
+    while (client.connected()) { //while we remain connected read out data coming back
+      client.readBytesUntil('\n',response,response_size);
+      if (serial) Serial.println(response);
+      if (strcmp(response,"\r")==0) { //found a blank line!
+        break;
+      }
+      memset(response, 0, response_size);
+      if (millis()-count>response_timeout) break;
+    }
+    memset(response, 0, response_size);  
+    count = millis();
+    while (client.available()) { //read out remaining text (body of response)
+      char_append(response,client.read(),OUT_BUFFER_SIZE);
+    }
+    if (serial) Serial.println(response);
+    client.stop();
+    if (serial) Serial.println("-----------");  
+  }else{
+    if (serial) Serial.println("connection failed :/");
+    if (serial) Serial.println("wait 0.5 sec...");
+    client.stop();
+  }
+}        
+
+uint8_t char_append(char* buff, char c, uint16_t buff_size) {
+  int len = strlen(buff);
+  if (len > buff_size) return false;
+  buff[len] = c;
+  buff[len + 1] = '\0';
+  return true;
+}
+
+//hand in a discharge amount, and generate a voltage
+double voltage_from_discharge(double discharge) {
+  double volt = -5.08321149 * discharge + 8.16962756;
+  volt = volt * discharge - 3.53049671;
+  volt = volt * discharge - 0.3295403;
+  volt = volt * discharge + 4.08151442;
+  return volt;
+}
+
+
+///nice binary-search...much quicker!
+double discharge_from_voltage(double voltage, double error){
+  if (voltage > 4.08){
+    return 0.0;
+  }
+  if (voltage < 3.31){
+    return 1.0;
+  }
+  double estimated_discharge = 0.5;
+  double top_bounds = 1.0;
+  double bottom_bounds = 0.0;
+  double estimated_volts = voltage_from_discharge(estimated_discharge);
+  while (fabs(estimated_volts- voltage) > error){
+    if (estimated_volts > voltage){
+      bottom_bounds = estimated_discharge;
+    }
+    else{
+      top_bounds = estimated_discharge;
+    }
+    estimated_discharge = (bottom_bounds + top_bounds)/2;
+    estimated_volts =  voltage_from_discharge(estimated_discharge);
+  } 
+  return estimated_discharge;
+}
